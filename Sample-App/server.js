10aac028ed864cbbbab250244cfb17abb997d726
Mirela Damyanova <mdamyanova181@gmail.com>
2020-07-15 16:41:39 +0300

    Add project

---
+++ Sample-App/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(8000);
+
+console.log("Running at Port 8000");
